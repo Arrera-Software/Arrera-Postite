2739b1084ae18a2ef91eb8f6c1da1d6fc0ffe05e
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-07-03 18:37:22 +0200

    Creation d'un attribut de la CUIAPostite pour la dection des OS

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     CUIAPostite w;
-    if (CDetectionOS().getosApple()){
+    if (w.osDect.getosApple()){
         QFile styleFile(":/style/MacOS.qss");
         if (styleFile.open(QIODevice::ReadOnly | QIODevice::Text)){
             w.setStyleSheet(QString::fromUtf8(styleFile.readAll()));
